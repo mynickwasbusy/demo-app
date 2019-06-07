2fce00062f2eaea56ea6454f3819576acf80777f
Kirill Osipatov <osipatovoff@gmail.com>
2019-06-07 15:41:03 +0300

    cofee

--- src/main/java/habr/demo/app/web/InfoEndpoint.java
+++ src/main/java/habr/demo/app/web/InfoEndpoint.java
@@ -27,7 +27,7 @@ public class InfoEndpoint {
     @GetMapping("/get")
     public Info getHi() {
         return new Info(
-                "Cofee!",
+                "Tea!",
                 version.getVersion(),
                 environment.getEnvironment()
 
